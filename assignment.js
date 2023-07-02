c29df58b51597e510793840cf83834789cd31672
Abishai Das <abishaidas2@gmail.com>
2023-07-02 17:13:19 +0530

    Updating for Jenkins again.

--- assignment.js
+++ assignment.js
@@ -2,8 +2,8 @@
 
 const express = require('express');
 
-const PORT = 3031
-const HOST = '0.0.0.0';
+var PORT = 3031
+var HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
@@ -14,4 +14,4 @@ app.listen(PORT, HOST);
 
 console.log('Assignment service up and running on ' + 'http://${HOST}:${PORT}');
 
-console.log('Version 1 - latest');
+console.log('Version 2 - latest');
